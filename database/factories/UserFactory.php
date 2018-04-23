67e83e490e74eb4ead782e64e658acddee89e8d3
Antonio Ramirez <anramirez@nolo.com>
2018-04-23 16:23:33 -0500

    Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,22 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Appointment::class, function (Faker $faker) {
+
+    $baseDate = Carbon::create(2018, 04, 25)->setTime(7,0,0);
+
+    $appointmentDate = $baseDate->addDays(rand(1,7))->addHours(rand(1,12))->format('Y-m-d H:i:s');
+
+    $notificationDate = (new Carbon($appointmentDate))->subMinutes(10)->format('Y-m-d H:i:s');
+
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'appointment_date' => $appointmentDate,
+        'notification_date' => $notificationDate,
+        'user_id' => 1,
     ];
 });
